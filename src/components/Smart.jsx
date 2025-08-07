c947be73472ac57a9930237c544fb5d149a981ed
isaaczanafria <isaaczanafria@gmail.com>
2025-08-07 00:11:12 -0500

    cambio

--- src/components/Smart.jsx
+++ src/components/Smart.jsx
@@ -7,23 +7,23 @@ export default function Smart() {
     <div class="w-full h-full absolute bg-red-600/50"></div>
 
         <div className='w-full flex flex-row items-center justify-center gap-1'>
-            <div className='flex flex-col items-center z-40 text-white gap-2'>
+            <div className='flex flex-col items-center z-40 text-white'>
                 <h2 className='text-8xl md:text-[175px] lg:text-[240px] xl:text-[300px] font-extrabold'>S</h2>
                 <p className='text-xs md:text-base lg:text-lg'>Especifico</p>
             </div>
-            <div className='flex flex-col items-center z-40 text-white gap-2'>
+            <div className='flex flex-col items-center z-40 text-white '>
                 <h2 className='text-8xl md:text-[175px] lg:text-[240px] xl:text-[300px] font-extrabold'>M</h2>
                 <p className='text-xs md:text-base lg:text-lg'>Medible</p>
             </div>
-            <div className='flex flex-col items-center z-40 text-white gap-2'>
+            <div className='flex flex-col items-center z-40 text-white '>
                 <h2 className='text-8xl md:text-[175px] lg:text-[240px] xl:text-[300px] font-extrabold'>A</h2>
                 <p className='text-xs md:text-base lg:text-lg'>Alcanzable</p>
             </div>
-            <div className='flex flex-col items-center z-40 text-white gap-2'>
+            <div className='flex flex-col items-center z-40 text-white '>
                 <h2 className='text-8xl md:text-[175px] lg:text-[240px] xl:text-[300px] font-extrabold'>R</h2>
                 <p className='text-xs md:text-base lg:text-lg'>Relevante</p>
             </div>
-            <div className='flex flex-col items-center z-40 text-white gap-2'>
+            <div className='flex flex-col items-center z-40 text-white '>
                 <h2 className='text-8xl md:text-[175px] lg:text-[240px] xl:text-[300px] font-extrabold'>T</h2>
                 <p className='text-xs md:text-base lg:text-lg'>Tiempo</p>
             </div>
